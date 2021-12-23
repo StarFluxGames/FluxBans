e7977c33e34203fb6927793758d3caaa50e92c4c
StarFluxGames <starfluxgames@gmail.com>
2021-12-24 01:09:43 +1100

    Initial commit

---
+++ src/main/java/com/starflux/fluxbans/FluxBans.java
@@ -0,0 +1,17 @@
+package com.starflux.fluxbans;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FluxBans extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
